8fb39ed306027f1f4efd660177209b7d4eb1cf07
aishaa741 <mahmoud.aisha@iisgalvanimi.edu.it>
2025-05-12 07:16:53 +0000

    punto 1

---
+++ progetto/src/app/foo/foo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foo',
+  imports: [],
+  templateUrl: './foo.component.html',
+  styleUrl: './foo.component.css'
+})
+export class FooComponent {
+
+}
